9d754a9a639eeabddc8925a5a9c44ec8dce60aee
Sergey Ozernyy <s.ozerny@avsw.ru>
2021-12-07 18:55:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/planetforme/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.planetforme.model
+
+import java.util.*
+
+data class User (val id:String = "0" ,
+            var firstName:String = "Ivan",
+            var lastNave:String = "Ivanov",
+            var avatar:String = ""
+            ) {}
